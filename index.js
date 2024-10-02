8e427bca58016fbe61ad9536f997695b0a2d0d11
kraza0 <faisalraza1570@gmail.com>
2024-10-02 17:53:32 +0500

    E_COMMERCE_STORE

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use("/images", express.static(uploadDir));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `${process.env.FRONTEND_URL}/images/${req.file.filename}`,
   });
 });
 
@@ -169,7 +169,7 @@ app.post("/signup", async (req, res) => {
     cart[i] = 0;
   }
   const user = new Users({
-    name: req.body.usename,
+    name: req.body.name,
     email: req.body.email,
     password: req.body.password,
     cartData: cart,
@@ -265,11 +265,26 @@ app.post("/removefromcart", fetchUser, async (req, res) => {
 });
 
 // Creating Endpoint to get cartdata
-app.post("/getcart", fetchUser, async (req,res) => {
-  console.log("GetCart");
-  let userData = await Users.findOne({ _id: req.user.id });
-  res.json(userData.cartData);
+app.post("/getcart", fetchUser, async (req, res) => {
+  try {
+    console.log("GetCart");
+    
+    // Fetch user data from the database using the user ID
+    let userData = await Users.findOne({ _id: req.user.id });
+
+    // Check if user data is null (i.e., user not found)
+    if (!userData) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Return the cartData if user exists
+    res.json(userData.cartData);
+  } catch (error) {
+    console.error("Error fetching cart:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
+
 
 // Starting the server
 app.listen(port, (error) => {
